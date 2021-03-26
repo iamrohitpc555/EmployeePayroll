03280dfae79720e7d1a03c252b2f0ac5dd967e35
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-26 23:35:27 +0530

    Update EmployeePayroll.js

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,1 +1,8 @@
 
+// Display Slider Values
+  const salary = document.querySelector('#salary');
+  const output = document.querySelector('.salary-output');
+  // output.textContent = salary.value;
+  salary.addEventListener('input', function () {
+      output.textContent = salary.value;
+  });
