93b449e083b94480845d7e6c271875ce99e552b4
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-26 23:38:41 +0530

    Update EmployeePayroll.js

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,4 +1,3 @@
-
 // Display Slider Values
   const salary = document.querySelector('#salary');
   const output = document.querySelector('.salary-output');
@@ -6,3 +5,34 @@
   salary.addEventListener('input', function () {
       output.textContent = salary.value;
   });
+
+  const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayRollData();
+    employeePayrollData.name = getInputValueById('#name');
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let year = getInputValueById('#year');
+    let month = parseInt(getInputValueById('#month'))-1;
+    let day = getInputValueById('#day');
+    employeePayrollData.startDate = new Date(year,month,day);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if(item.checked)
+        selItems.push(item.value);
+    });
+    return selItems;
+}
