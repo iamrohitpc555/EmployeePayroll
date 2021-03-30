a645752d68666a39cc766983a6b74ed1dfbf7d83
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-30 23:30:08 +0530

    Create employee_from.js

---
+++ js/employee_from.js
@@ -0,0 +1,59 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener("input", function() {
+        output.textContent = salary.value;
+    });
+
+});
+
+const save = () => {
+    let employeePayrollData;
+    try {
+        employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+        // employeePayrollData.name = getInputValueById('#name');
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.startDate = new Date(date);
+    console.log(date);
+    alert(employeePayrollData.toString());
+    // alert("Welcome");
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
