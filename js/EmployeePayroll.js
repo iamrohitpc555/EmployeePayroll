4c0f809fc34813e34e61cca583ef198a53bbc0f3
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-01 20:44:25 +0530

    Update EmployeePayroll.js

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,28 +1,28 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-       if(name.value.length == 0){
-           textError.textContent = "";
-           return;
-       } 
-       try{
-           (new EmployeePayrollData()).name = name.value;
-           textError.textContent = "";
-       }catch(e){
-           textError.textContent = e;
-       }
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
     });
     const salary = document.querySelector("#salary");
     const output = document.querySelector(".salary-output");
     output.textContent = salary.value;
-    salary.addEventListener("input", function() {
+    salary.addEventListener("input", function () {
         output.textContent = salary.value;
     });
 
     const date = document.querySelector('#startdate');
     const dateError = document.querySelector(".date-error");
-    date.addEventListener('input', function() {
+    date.addEventListener('input', function () {
         const startdate = new Date(Date.parse(getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year')));
         try {
             (new EmployeePayrollData()).startDate = startdate;
@@ -43,7 +43,6 @@ const save = () => {
     try {
         employeePayrollData = createEmployeePayroll();
     } catch (e) {
-        console.log(e);
         return;
     }
 }
@@ -65,7 +64,6 @@ const createEmployeePayroll = () => {
     employeePayrollData.startDate = new Date(date);
     console.log(date);
     alert(employeePayrollData.toString());
-    // alert("Welcome");
     return employeePayrollData;
 }
 
@@ -81,5 +79,10 @@ const getSelectedValues = (propertyValue) => {
 
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
     return value;
 }
