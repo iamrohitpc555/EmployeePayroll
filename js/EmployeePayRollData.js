ebe1011e5e50903f37d42b5b65a522c832f9ac89
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-04 22:22:49 +0530

    Update EmployeePayRollData.js

--- js/EmployeePayRollData.js
+++ js/EmployeePayRollData.js
@@ -1,106 +1,72 @@
-class EmployeePayRollData{
+class EmployeePayrollData {
 
-    get id()
-    {
+    //Getters & Setters
+    get id() {
         return this._id;
     }
-
-    set id(id)
-    {
-        let idRegex = RegExp('[1-9]{1}[0-9]*');
-        if(idRegex.test(id))
+    set id(id) {
         this._id = id;
-        else
-        throw 'Id is incorrect';
     }
-
-    get name()
-    {
+    get name() {
         return this._name;
     }
-
-    set name(name)
-    {
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if(nameRegex.test(name))
-        this._name = name;
-        else 
-        throw 'Name is incorrect';
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}([ ][A-Z]{1}[a-zA-Z]{2,}){0,}$');
+        if (nameRegex.test(name)) {
+            this._name = name;
+        } else {
+            throw 'Invalid Name Format';
+        }
     }
-
-    get profilePic()
-    {
+    get profilePic() {
         return this._profilePic;
     }
-
-    set profilePic(profilePic)
-    {
+    set profilePic(profilePic) {
         this._profilePic = profilePic;
     }
-
-    get gender()
-    {
+    get gender() {
         return this._gender;
     }
-
-    set gender(gender)
-    {
+    set gender(gender) {
         this._gender = gender;
     }
-
-    get department()
-    {
+    get department() {
         return this._department;
     }
-
-    set department(department)
-    {
+    set department(department) {
         this._department = department;
     }
-
-    get salary()
-    {
+    get salary() {
         return this._salary;
     }
-
-    set salary(salary)
-    {
+    set salary(salary) {
         this._salary = salary;
     }
-
-    get note()
-    {
+    get note() {
         return this._note;
     }
-
-    set note(note)
-    {
+    set note(note) {
         this._note = note;
     }
-
-    get startDate()
-    {
+    get startDate() {
         return this._startDate;
     }
-
-    set startDate(startDate)
-    {
-        let datee = new Date();
-        if(startDate<=datee)
-        {
+    set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now)
+            throw 'Date is a Future Date';
+        // var diff = Math.abs(now.getTime() - startDate.getTime());
+        // if (diff / (1000 * 60 * 60 * 24) > 30)
+        //     throw 'Start Date Is Beyond 30 Days !';
         this._startDate = startDate;
-        }
-        else
-        throw 'StratDate is incorrect';
     }
 
-    toString()
-    {
-        const format = {year:'numeric', month:'long', day:'numeric'};
-        const date = this.startDate === undefined ? "undefined" :
-                     this.startDate.toLocaleDateString("en-US",format);
-        return "Id = "+this.id+", Name = "+this.name+", Gender = "+this.gender+", ProfilePic = "+this.profilePic+", Department = "+this.department+", Salary = "+this.salary+
-                ", StartDate = "+date+", Note = "+this.note;
+    //Method
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "Undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id : " + this.id + " Name : " + this.name + " Gender : " + this.gender + " Profile Pic : " +
+            this.profilePic + " Department : " + this.department + " Salary : " + this.salary + " Start Date : " + empDate +
+            " Notes : " + this.note;
     }
-
 }
