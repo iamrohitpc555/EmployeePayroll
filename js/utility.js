59ece9740974f10e2afe3b1aa90353a41b7c4b92
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-06 22:16:05 +0530

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -1,6 +1,20 @@
 const stringifyDate = (date) => {
-    const options = { day: 'numeric', month: 'short', year: 'numeric'};
+    const options = { day: 'numeric', month: 'long', year: 'numeric'};
     const newDate = !date ? "undefined" :
                     new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
     return newDate;
+}
+
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}([ ][A-Z]{1}[a-zA-Z]{2,}){0,}$');
+    if (!nameRegex.test(name))
+        throw 'Invalid Name Format';
+}
+
+const checkStartDate = (startDate) =>{
+    let now = new Date();
+    if (startDate > now) throw 'Date is a Future Date';
+    var diff = Math.abs(now.getTime() - startDate.getTime());
+    if (diff / (1000 * 60 * 60 * 24) > 30)
+        throw 'Start Date Is Beyond 30 Days !';
 }
