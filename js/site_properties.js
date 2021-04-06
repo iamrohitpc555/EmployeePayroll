2602d3bd28f696269dcd9f870ced54f8a2bcbc12
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-06 22:15:35 +0530

    Update site_properties.js

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "../pages/home.html",
-    add_employee_page:"../pages/employee_form.html"
+    add_employee_page:"../pages/employee_form.html",
+    server_url: "http://localhost:3004/EmployeePayrollDB/"
 };
